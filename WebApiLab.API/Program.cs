666480f79cbf9d6c91d95865a5fa48a7fe40cbfe
Jim <jimkernicky@gmail.com>
2026-07-02 14:35:41 -0400

    add swagger and resources json file

--- WebApiLab.API/Program.cs
+++ WebApiLab.API/Program.cs
@@ -1,41 +1,34 @@
+using System.Text.Json;
+using WebApiLab.API.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+string jsonFile = File.ReadAllText("./Resources/64KB.json");
+
+var jsonData = JsonSerializer.Deserialize<List<Person>>(
+    jsonFile,
+    new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+app.MapGet(pattern: "/people", handler: () => jsonData)
+    .WithName(endpointName: "GetPeople")
+    .WithOpenApi()
+    .Produces<List<Person>>(statusCode: StatusCodes.Status200OK);
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
