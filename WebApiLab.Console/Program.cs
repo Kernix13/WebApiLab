bff337a92e8bb921c07ddb5490d7a22124a04f05
Jim <jimkernicky@gmail.com>
2026-07-02 15:09:56 -0400

    add console project

---
+++ WebApiLab.Console/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json;
+using WebApiLab.Console.Models;
+
+HttpClient client = new HttpClient();
+
+client.BaseAddress = new Uri("http://localhost:5195");
+
+HttpResponseMessage response = await client.GetAsync("/people");
+// HttpResponseMessage response = await client.GetAsync("/api/People");
+
+if (response.IsSuccessStatusCode)
+{
+    string jsonResponse = await response.Content.ReadAsStringAsync();
+
+    var people = JsonSerializer.Deserialize<List<Person>>(
+        jsonResponse,
+        new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+    foreach (var person in people)
+    {
+        Console.WriteLine($"{person.Name} speaks {person.Language}");
+    }
+}
+else
+{
+    Console.WriteLine($"Error: {response.StatusCode}");
+    Console.WriteLine(await response.Content.ReadAsStringAsync());
+}
